17dbb4372f8b4ca5329ecc79484d3c46faa482e5
Laura Wentzell Ahmad <laura.s.ahmad@gmail.com>
2017-10-12 20:37:07 -0500

    changed and cleaned
    array now ojects

--- arrays.js
+++ arrays.js
@@ -2,19 +2,16 @@ var nutrients = {
 	iron: {
  	code: "303",
  	discription: "Iron is an essential element for blood production. About 70 percent of your body's iron is found in the red blood cells of your blood called hemoglobin and in muscle cells called myoglobin. Hemoglobin is essential for transferring oxygen in your blood from the lungs to the tissues. Myoglobin, in muscle cells, accepts, stores, transports and releases oxygen. About 6 percent of body iron is a component of certain proteins, essential for respiration and energy metabolism, and as a component of enzymes involved in the synthesis of collagen and some neurotransmitters. Iron also is needed for proper immune function.About 25 percent of the iron in the body is stored as ferritin, found in cells and circulates in the blood. The average adult male has about 1,000 mg of stored iron (enough for about three years), whereas women on average have only about 300 mg (enough for about six months). When iron intake is chronically low, stores can become depleted, decreasing hemoglobin levels. When iron stores are exhausted, the condition is called iron depletion. Further decreases may be called iron-deficient erythropoiesis and still further decreases produce iron deficiency anemia. Blood loss is the most common cause of iron deficiency. In men and postmenopausal women, iron deficiency is almost always the result of gastrointestinal blood loss. In menstruating women, genitourinary blood loss often accounts for increased iron requirements. Oral contraceptives tend to decrease menstrual blood loss, whereas intrauterine devices tend to increase menstrual bleeding. Other causes of genitourinary bleeding and respiratory tract bleeding also increase iron requirements."
-}},
-{
+},
 	calcium: {
  	code: "301",
  	discription: "Calcium, the most abundant mineral in the body, is found in some foods, added to others, available as a dietary supplement, and present in some medicines (such as antacids). Calcium is required for vascular contraction and vasodilation, muscle function, nerve transmission, intracellular signaling and hormonal secretion, though less than 1% of total body calcium is needed to support these critical metabolic functions. Serum calcium is very tightly regulated and does not fluctuate with changes in dietary intakes; the body uses bone tissue as a reservoir for, and source of calcium, to maintain constant concentrations of calcium in blood, muscle, and intercellular fluids. The remaining 99% of the body’s calcium supply is stored in the bones and teeth where it supports their structure and function. Bone itself undergoes continuous remodeling, with constant resorption and deposition of calcium into new bone. The balance between bone resorption and deposition changes with age. Bone formation exceeds resorption in periods of growth in children and adolescents, whereas in early and middle adulthood both processes are relatively equal. In aging adults, particularly among postmenopausal women, bone breakdown exceeds formation, resulting in bone loss that increases the risk of osteoporosis over time."
-}},
-{
+},
 	magnesium: {
  	code: "304",
  	discription: "still to be added"
-}},
-{
+},
 	potassium: {
  	code: "306",
  	discription: "still to be added"
-}},
+}};
