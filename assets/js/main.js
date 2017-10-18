cea821ad9f226b405c0a8f5f2d18c9d55b4760a2
Laura Wentzell Ahmad <laura.s.ahmad@gmail.com>
2017-10-17 21:09:09 -0500

    added 2 more
    adding two

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,6 +58,10 @@ $( document ).ready(function() {
   "phosphorus": {
     code: "305",
     discription: "Next to calcium, phosphorus is the most abundant mineral in the body. These 2 important nutrients work closely together to build strong bones and teeth. About 85% of the body's phosphorus is in bones and teeth. Phosphorous is also present in smaller amounts in cells and tissues throughout the body. Phosphorus helps filter out waste in the kidneys and plays an essential role in how the body stores and uses energy. It also helps reduce muscle pain after a workout. Phosphorus is needed for the growth, maintenance, and repair of all tissues and cells, and for the production of the genetic building blocks, DNA and RNA. Phosphorus is also needed to help balance and use other vitamins and minerals, including vitamin D, iodine, magnesium, and zinc."
+},
+  "selenium": {
+    code: "317",
+    discription: "Selenium is used for diseases of the heart and blood vessels, including stroke and “hardening of the arteries” (atherosclerosis). It is also used for preventing various cancers including cancer of the prostate, stomach, lung, and skin. Some people use selenium for under-active thyroid, osteoarthritis, rheumatoid arthritis (RA), an eye disease called macular degeneration, hay fever, infertility, cataracts, gray hair, abnormal pap smears, chronic fatigue syndrome (CFS), mood disorders, arsenic poisoning, and preventing miscarriage. Selenium is also used for preventing serious complications and death from critical illnesses such as head injury and burns. It is also used for preventing bird flu, treating HIV/AIDS, and reducing side effects from cancerchemotherapy."
 }};
 
   // all other variables
